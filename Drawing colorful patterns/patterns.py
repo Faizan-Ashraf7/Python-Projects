e0856e727c1d172b9f4d93d66dbc86d24b511fd2
Faizan Ashraf7 <faixannajar71@gmail.com>
2023-10-07 22:43:33 +0530

    Added new pattern

--- Drawing colorful patterns/patterns.py
+++ Drawing colorful patterns/patterns.py
@@ -1,9 +1,9 @@
 import turtle
 colors=['red','yellow','blue','orange']
-turtle.speed(0)
+turtle.speed(0.3)
 turtle.bgcolor('black')
 for i in range(360):
   turtle.pencolor(colors[i% len(colors)])
   turtle.forward(i)
-  turtle.right(121)
+  turtle.right(150)
 turtle.exitonclick()
