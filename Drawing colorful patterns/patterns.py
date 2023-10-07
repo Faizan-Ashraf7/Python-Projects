92b7ad1ec2f76955ad616ad8d5454ee0212b03c0
Faizan Ashraf7 <faixannajar71@gmail.com>
2023-10-07 22:40:04 +0530

    Added spiral

--- Drawing colorful patterns/patterns.py
+++ Drawing colorful patterns/patterns.py
@@ -1,2 +1,9 @@
 import turtle
 colors=['red','yellow','blue','orange']
+turtle.speed(0)
+turtle.bgcolor('black')
+for i in range(360):
+  turtle.pencolor(colors[i% len(colors)])
+  turtle.forward(i)
+  turtle.right(121)
+turtle.exitonclick()
